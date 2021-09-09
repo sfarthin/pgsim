47468e37b1cf8f9369fd11596cb0f21f5b24aa93
Steve Farthing <stevefarthing86@gmail.com>
2021-09-08 22:38:09 -0400

    The next issue

--- fixtures/parseAndFormat/15-selectStmt.join.sql
+++ fixtures/parseAndFormat/15-selectStmt.join.sql
@@ -2,3 +2,4 @@ SELECT * FROM foo JOIN bar ON bar.foo_id = foo.id AND foo.created_at = NOW();
 SELECT * FROM foo LEFT JOIN bar ON bar.foo_id = foo.id AND foo.created_at = NOW();
 SELECT * FROM foo INNER JOIN bar ON bar.foo_id = foo.id AND foo.created_at = NOW();
 SELECT * FROM foo RIGHT JOIN bar b ON b.foo_id = foo.id AND foo.created_at = NOW();
+SELECT * FROM foo LEFT JOIN (SELECT 1) as sq ON 1 = 1;
